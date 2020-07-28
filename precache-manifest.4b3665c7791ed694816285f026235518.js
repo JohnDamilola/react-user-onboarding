f661f34f94c72485ad7884c45fcf5df68aa28383
JohnDamilola <johndamilola03@gmail.com>
2020-07-28 01:14:29 +0100

    Updates

---
+++ precache-manifest.4b3665c7791ed694816285f026235518.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f088a6b9ae2fdde5b45650c45454d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a72c48d3e171cf16390f",
+    "url": "./static/css/main.09a4bf26.chunk.css"
+  },
+  {
+    "revision": "8e1380d90c649f59e3d1",
+    "url": "./static/js/2.44327ae8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.44327ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72c48d3e171cf16390f",
+    "url": "./static/js/main.06541a43.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.06541a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf03b237032832a396b2",
+    "url": "./static/js/runtime-main.c3cfbd4e.js"
+  },
+  {
+    "revision": "5e88ea2bfc9bac5b59738ad6b816ef03",
+    "url": "./static/media/plus.5e88ea2b.svg"
+  }
+]);
