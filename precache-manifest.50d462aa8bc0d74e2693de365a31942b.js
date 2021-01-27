e8580c60b438aeec0e12b858c77fcb15bb161eb5
John Damilola <johndamilola03@gmail.com>
2021-01-27 08:23:05 +0100

    Updates

---
+++ precache-manifest.50d462aa8bc0d74e2693de365a31942b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798b6224ad6162f99722b57edb8e454c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3cbf21084fb7da2c68e",
+    "url": "./static/css/main.7a3e5759.chunk.css"
+  },
+  {
+    "revision": "6dfbdc809d570124a74a",
+    "url": "./static/js/2.d5c76e48.chunk.js"
+  },
+  {
+    "revision": "2a5652ef9634c9bfbd1c095b3bb03a14",
+    "url": "./static/js/2.d5c76e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3cbf21084fb7da2c68e",
+    "url": "./static/js/main.fec9799e.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.fec9799e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf03b237032832a396b2",
+    "url": "./static/js/runtime-main.c3cfbd4e.js"
+  },
+  {
+    "revision": "5e88ea2bfc9bac5b59738ad6b816ef03",
+    "url": "./static/media/plus.5e88ea2b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c7b80152027bd0662bd5a06e31771e0.js"
+  "./precache-manifest.50d462aa8bc0d74e2693de365a31942b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
